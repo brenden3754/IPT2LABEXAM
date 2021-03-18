2685096f576a167532846ce511bc6d05151ac2f4
brenden <aguilarbrenden@gmail.com>
2021-03-18 23:08:00 +0800

    IPT2 LABORATORY EXAM

--- NetBeansProjects/PrintIntegers/src/printintegers/PrintIntegers.java
+++ NetBeansProjects/PrintIntegers/src/printintegers/PrintIntegers.java
@@ -3,7 +3,7 @@ package printintegers;
 
 
 public class PrintIntegers {
-        public void integers(){
+ public void integers(){
         int c;
         for (c=1;c<=5;c++){
             System.out.println(c);
@@ -14,34 +14,47 @@ public class PrintIntegers {
     public void Reverseorder(){
         int d;
         for (d=5;d>=0;d--){
-            System.out.println("Print number in a reverse order:"+d);
+            System.out.println("A Reverse Order:"+d);
         }
     }
     public void Leader(){
         int a;
         System.out.println("Hi, I'm Brenden Aguilar");
           System.out.println("Leader in IPT2");
-            System.out.println("This is our simple program");
+            
         
     }
     public void member1(){
         int b;
         System.out.println("I'm Angelica Guitierrez");
         System.out.println("From San Miguel Tarlac City");
+           System.out.println("Part of this group");
+           System.out.println("\n");
+           System.out.println("This is our simple program");
+        
+    
+    }
+     public void member2(){
+        int j;
+        
         System.out.println("End of our Program");
     
     }
+  
     
     public static void main(String[] args) {
         PrintIntegers objint = new PrintIntegers();
-        
         objint.Leader();
+           System.out.println("\n");
+        objint.member1();
+           System.out.println("\n");
         objint.integers();
         objint.Reverseorder();
    
         
-        System.out.println("Task i complete!");
-        objint.member1();
+            System.out.println("Task i complete!");
+            System.out.println("\n");
+        objint.member2();
     }
     
 }
