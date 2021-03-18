8b984f5c62b1b745f69d5975f57cc73d562a8337
brenden <aguilarbrenden@gmail.com>
2021-03-18 22:39:59 +0800

    IPT2_LABEXAM

--- NetBeansProjects/PrintIntegers/src/printintegers/PrintIntegers.java
+++ NetBeansProjects/PrintIntegers/src/printintegers/PrintIntegers.java
@@ -5,7 +5,7 @@ package printintegers;
 public class PrintIntegers {
         public void integers(){
         int c;
-        for (c=1;c<=10;c++){
+        for (c=1;c<=5;c++){
             System.out.println(c);
         }
         System.out.println("-------------------------");
@@ -13,18 +13,35 @@ public class PrintIntegers {
     
     public void Reverseorder(){
         int d;
-        for (d=10;d>=0;d--){
+        for (d=5;d>=0;d--){
             System.out.println("Print number in a reverse order:"+d);
         }
     }
-  
+    public void Leader(){
+        int a;
+        System.out.println("Hi, I'm Brenden Aguilar");
+          System.out.println("Leader in IPT2");
+            System.out.println("This is our simple program");
+        
+    }
+    public void member1(){
+        int b;
+        System.out.println("I'm Angelica Guitierrez");
+        System.out.println("From San Miguel Tarlac City");
+        System.out.println("End of our Program");
+    
+    }
+    
     public static void main(String[] args) {
         PrintIntegers objint = new PrintIntegers();
         
+        objint.Leader();
         objint.integers();
         objint.Reverseorder();
+   
         
         System.out.println("Task i complete!");
+        objint.member1();
     }
     
 }
